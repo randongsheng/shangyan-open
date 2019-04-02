04310b5848bb6a3c2951475e530f8cfc42cd5cce
zsc <zhangshichong3@163.com>
2019-04-02 14:57:23 +0800

    bug

--- application/index/controller/Label.php
+++ application/index/controller/Label.php
@@ -119,7 +119,7 @@ class Label extends Base
 		$request = Request::instance();
 		$post = [];
 		$post['master_id'] = input('master_id');
-		$post['obey_id'] = input('obey_id','');
+		$post['obey_id'] = $request->post('obey_id');
 		if(empty($post['master_id'])){
 			return json(['success'=>false,'code'=>'002','message'=>'标签ID不能为空']);
 		}
