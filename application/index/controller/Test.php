8b75905b875a217de2090ebebebd78f3200db3ea
Whlin <316226514@qq.com>
2019-07-01 17:20:40 +0800

    测试

---
+++ application/index/controller/Test.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\index\controller;
+use think\Request;
+use app\index\model\Admin;
+use app\index\model\User as UserModel;
+use app\index\model\Teacher;
+use app\index\model\Clinic;
+use app\index\model\Blacklist;
+use think\Session;
+use think\Db;
+use think\Validate;
+
+class Test extends Base
+{
+
+    public function index()
+    {
+        echo  '测试成功';
+
+    }
+
+
+
+}
