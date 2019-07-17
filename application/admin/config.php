90c42122184ecbbad73517824c9ce3e6483adca6
Whlin <316226514@qq.com>
2019-07-17 16:58:11 +0800

    测试上传图片

---
+++ application/admin/config.php
@@ -0,0 +1,60 @@
+<?php 
+return [
+	// 本站访问协议
+	'protocol'=>'http://',
+    // 尚言心里地址
+    'SYURL'=>'api.sc-cg.cn',
+    // 存储地址
+    'save_protocol'=>'http://',
+    'save_url'=>'oss.shangyanxinli.com/',
+    
+	/*'IMG'=>[
+		// 机构logo
+		'clinic_logo'=>'./uploads/clinic/logo/',
+		// 机构营业执照
+		'clinic_business_license'=>'./uploads/clinic/business_license/',
+		// 机构相关人员的身份证信息
+		'clinic_identity'=>'./uploads/clinic/identity/',
+		// 机构场地信息
+		'clinic_scene'=>'./uploads/clinic/scene/',
+		// 机构其他附加信息的图片
+		'clinic_related'=>'./uploads/clinic/related/',
+		// 机构保证金支付二维码
+		'clinic_qecode'=>'./uploads/clinic/bond_qrcode/',
+	],*/
+	'IMG'=>[
+		// 机构logo
+		'clinic_logo'=>'clinic/logo/',
+		// 机构营业执照
+		'clinic_business_license'=>'clinic/business_license/',
+		// 机构相关人员的身份证信息
+		'clinic_identity'=>'clinic/identity/',
+		// 机构场地信息
+		'clinic_scene'=>'clinic/scene/',
+		// 机构其他附加信息的图片
+		'clinic_related'=>'clinic/related/',
+		// 机构保证金支付二维码
+		'clinic_qrcode'=>'clinic/bond_qrcode/',
+		// 老师证书
+		'teacher_certificate'=>'clinic/teacher/certificate/',
+		// 老师学历
+		'teacher_education'=>'clinic/teacher/education/',
+		// 老师培训经历
+		'teacher_train'=>'clinic/teacher/train/',
+		// 老师上身照
+		'teacher_body'=>'clinic/teacher/body/',
+	],
+	/*'IMGPRESENT'=>[
+		'clinic_logo'=>'clinic/logo/',
+		'clinic_business_license'=>'clinic/business_license/',
+		'clinic_identity'=>'clinic/identity/',
+		'clinic_scene'=>'clinic/scene/',
+		'clinic_related'=>'clinic/related/',
+		'clinic_qecode'=>'clinic/bond_qrcode/',
+	],*/
+
+	'deoisit'=>1000,
+
+	// 保证金白名单
+	'white_list'=>[51,],
+];
